ffad24965f2a654fd6b39209abbdf595e9a612a7
Vitor Saborito <110786874+VituuSaborito@users.noreply.github.com>
2025-05-14 10:51:25 -0300

    [style]: create page login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,6 +3,9 @@ import Swal from "sweetalert2";
 import Input from "../components/Input";
 import Button from "../components/Button";
 import Auth from "../server/routes/auth";
+import '../style/Login.scss';
+
+
 
 const auth = new Auth()
 
@@ -43,7 +46,7 @@ const Login = () => {
 
     return(
         <div className="content">
-            <div className="p-lf"></div>
+            
 
             <div className="p-rt">
                 <div className="box">
